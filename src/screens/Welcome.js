8f8f50bc09d77090e0e5dc75b9709e575f8f7274
Odetola Azeez Opeyemi <yemiOtola@gmail.com>
2020-03-10 10:02:51 +0100

    welcome screen...

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,22 +1,69 @@
 import React, { Component } from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, FlatList, Image, Dimensions, Animated } from 'react-native'
 import { Button, Block, Text } from '../components';
 import { theme } from '../constants';
 
 export class Welcome extends Component {
+  scrollX = new Animated.Value(0);
+
+  state = {
+
+  }
 
   renderIllustrations() {
+    const { illustrations } = this.props;
+    const { width, height } = Dimensions.get('window');
     return (
-      <Block>
-        <Text>Image</Text>
-      </Block>
+      <FlatList
+        horizontal
+        pagingEnabled
+        scrollEnabled
+        showsHorizontalScrollIndicator={false}
+        scrollEventThrottle={16}
+        snapToAlignment="center"
+        data={illustrations}
+        extraDate={this.state}
+        keyExtractor={(item, index) => `${item.id}`}
+        renderItem={({ item }) => (
+          <Image
+            source={item.source}
+            resizeMode="contain"
+            style={{ width, height: height / 2, overflow: 'visible' }}
+          />
+        )}
+        onScroll={
+          Animated.event([{
+            nativeEvent: {
+              contentOffset: { x: this.scrollX }
+            }
+          }])
+        }
+      />
     )
   }
 
   renderSteps() {
+    const { illustrations } = this.props;
+    const { width } = Dimensions.get('window');
+    const stepPosition = Animated.divide(this.scrollX, width);
     return (
-      <Block>
-        <Text>* * *</Text>
+      <Block row center middle style={styles.stepsContainer}>
+        {illustrations.map((item, index) => {
+          const opacity = stepPosition.interpolate({
+            inputRange: [index - 1, index, index + 1],
+            outputRange: [0.4, 1, 0.4],
+            extrapolate: 'clamp',
+          });
+          return (
+            <Block
+              animated
+              flex={false}
+              key={`step-${index}`}
+              color="gray"
+              style={[styles.steps, { opacity }]}
+            />
+          )
+        })}
       </Block>
     )
   }
@@ -52,9 +99,29 @@ export class Welcome extends Component {
   }
 }
 
+Welcome.defaultProps = {
+  illustrations: [
+    { id: 1, source: require('../../assets/images/illustration_1.png') },
+    { id: 2, source: require('../../assets/images/illustration_2.png') },
+    { id: 3, source: require('../../assets/images/illustration_3.png') },
+  ],
+};
+
 const styles = StyleSheet.create({
   textEnjoy: {
     marginTop: theme.sizes.padding
-  }
+  },
+  stepsContainer: {
+    position: 'absolute',
+    right: 0,
+    left: 0,
+    bottom: theme.sizes.base * 3
+  },
+  steps: {
+    width: 5,
+    height: 5,
+    borderRadius: 5,
+    marginHorizontal: 2.5,
+  },
 })
 export default Welcome;
